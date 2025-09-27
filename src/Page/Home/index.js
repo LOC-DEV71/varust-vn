ea4411fde6bdd6e83a9e01ee208492f4581a9eef
Lam Chi Loc <loclc8533@ut.edu.vn>
2025-09-27 18:36:40 +0700

    update top home

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -6,45 +6,66 @@ import SectionThree from "./section/section-3";
 import SectionFour from "./section/section-4";
 import SectionFive from "./section/section-5";
 import SectionSix from "./section/section-6";
+import { useEffect, useState } from "react";
 
 function Home(){
+    const [collapse, setCollapse] = useState(true);
+    useEffect(() => {
+            const handleResize = () => {
+            if (window.innerWidth <= 855) {
+                setCollapse(false);
+            } else {
+                setCollapse(true);
+            }
+            };
+    
+            // chạy 1 lần khi load
+            handleResize();
+    
+            // lắng nghe resize
+            window.addEventListener("resize", handleResize);
+            return () => window.removeEventListener("resize", handleResize);
+        }, []);
     return(
         <>
             <div className="layout__main-top">
-                <Link to={"updating"}>
+                <Link to={"updating"} title="Build PC tặng màn 240Hz">
                     <TagOutlined />
                     <div className="layout__main-top-title">
-                        Build PC tặng màn 240Hz
+                        {collapse ? "Build PC tặng màn 240Hz":"Build PC"}
                     </div>
                 </Link>
-                <Link to={"updating"}>
+                <Link to={"updating"} title="Tin tức công nghệ">
                     <FileTextOutlined />
                     <div className="layout__main-top-title">
-                        Tin tức công nghệ
+                        {collapse ? "Tin tức công nghệ" : "Tin Tức"}
                     </div>
                 </Link>
-                <Link to={"updating"}>
+                <Link to={"updating"} title="Dịch vụ sửa chữa">
                     <SettingOutlined />
                     <div className="layout__main-top-title">
-                        Dịch vụ sửa chữa
+                        {collapse ? "Dịch vụ sửa chữa" : "Sửa Chữa"}
                     </div>
                 </Link>
-                <Link to={"updating"}>
+                <Link to={"updating"} title="Dịch vụ kĩ thuật tại nhà">
                     <PushpinOutlined />
                     <div className="layout__main-top-title">
-                        Dịch vụ kĩ thuật tại nhà
+                        {collapse ? " Dịch vụ kĩ thuật tại nhà":"Dịch vụ"}
+                       
                     </div>
                 </Link>
-                <Link to={"updating"}>
+                <Link to={"updating"} title="Thay cũ đổi mới">
                     <DollarOutlined />
                     <div className="layout__main-top-title">
-                        Thay cũ đổi mới
+                        {collapse ? "Thay cũ đổi mới":"Đổi"}
+                        
                     </div>
                 </Link>
-                <Link to={"updating"}>
+                <Link to={"updating"} title="Tran cứu bảo hành">
                     <SecurityScanOutlined />
                     <div className="layout__main-top-title">
-                        Tra cứu bảo hành
+                        {collapse ? "Tra cứu bảo hành" : "Tra cứu"}
+                        
                     </div>
                 </Link>
             </div>
